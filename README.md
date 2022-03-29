c142cc6c6577bd22ddb0b74f4c590d568409e763
Julio Betta <juliobetta@gmail.com>
2022-03-28 23:25:13 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Solidity example project: Lottery
+
+Example project from [Udemy's Ethereum and Solidity: The Complete Developer's Guide
+](https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide)
---
+++ test/lottery.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { abi, evm } = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      // arguments: ???,
+    })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+});
